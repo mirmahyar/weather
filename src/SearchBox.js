18a63784dba68777e09c0d5c181bc6cfed611a16
mirmahyar <m.mirsamadi@gmail.com>
2020-09-29 18:29:33 +0330

    completed API call section and display a very basic result back to user

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SearchBox() {
+function SearchBox() {
+
+    const [query, setQuery] = useState('');
+    const [weather, setWeather] = useState('');
+
+    const SearchWeather = async (e) => {
+        e.preventDefault();
+        console.log("Submitting...")
+
+
+        const key = "0443b36ad2bf74d27d89bf3bbe003dd5"
+
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=0443b36ad2bf74d27d89bf3bbe003dd5`;
+        // const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+        try {
+            const resp = await fetch(url);
+            const data = await resp.json();
+            console.log(data);
+            setWeather(data.weather[0].description);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+
+
     return (
-        <form className="form">
-            <label className="label" htmlFor="query">City Name: </label>
-            <input className="input" type="text" name="query"
-                placeholder="i.e. Tehran" />
-            <button className="button" type="submit">Search</button>
-        </form>
+        <div>
+            <form className="form" onSubmit={SearchWeather} >
+                <label className="label" htmlFor="query">City name : </label>
+                <input className="input" type="text" placeholder="i.e Tehran"
+                    value={query} onChange={(e) => setQuery(e.target.value)} />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <p className="displayResult">
+                {weather}
+            </p>
+
+
+        </div>
     )
 }
+
+
+
+export default SearchBox
