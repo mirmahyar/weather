cdefbddd5ad14e8f047aeaa4d228aac718757779
mirmahyar <m.mirsamadi@gmail.com>
2020-09-30 17:37:26 +0330

    designing the result a lil bit

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 function SearchBox() {
 
     const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState(0);
+    const [weather, setWeather] = useState('');
 
     const SearchWeather = async (e) => {
         e.preventDefault();
         console.log("Submitting...")
 
 
-        const key = "0443b36ad2bf74d27d89bf3bbe003dd5"
+        //const key = "0443b36ad2bf74d27d89bf3bbe003dd5"
 
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=0443b36ad2bf74d27d89bf3bbe003dd5`;
         // const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
@@ -18,7 +18,7 @@ function SearchBox() {
             const resp = await fetch(url);
             const data = await resp.json();
             console.log(data);
-            setWeather(data.coord.lat);
+            setWeather(data.weather[0].description);
         } catch (err) {
             console.error(err);
         }
@@ -34,7 +34,7 @@ function SearchBox() {
                     value={query} onChange={(e) => setQuery(e.target.value)} />
                 <button className="button" type="submit">Search</button>
             </form>
-            <p>
+            <p className="description">
                 {weather}
             </p>
 
