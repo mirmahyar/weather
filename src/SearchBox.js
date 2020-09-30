b0f562bbe60208460201b741f4fac9138f9a598d
mirmahyar <m.mirsamadi@gmail.com>
2020-09-30 18:50:06 +0330

    cleaning the code

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -9,11 +9,8 @@ function SearchBox() {
         e.preventDefault();
         console.log("Submitting...")
 
-
-        //const key = "0443b36ad2bf74d27d89bf3bbe003dd5"
-
         const url = `http://api.openweathermap.org/data/2.5/weather?q=${query}&appid=0443b36ad2bf74d27d89bf3bbe003dd5`;
-        // const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
         try {
             const resp = await fetch(url);
             const data = await resp.json();
@@ -23,8 +20,6 @@ function SearchBox() {
             console.error(err);
         }
     }
-
-
 
     return (
         <div>
@@ -37,8 +32,6 @@ function SearchBox() {
             <p className="description">
                 {weather}
             </p>
-
-
         </div>
     )
 }
@@ -46,3 +39,5 @@ function SearchBox() {
 
 
 export default SearchBox
+
+
